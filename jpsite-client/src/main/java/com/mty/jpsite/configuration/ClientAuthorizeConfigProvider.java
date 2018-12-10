d9a567f74aea1e29dfa20839552d4860c58f9790
jiangpeng <jiangpeng@iplas.com.cn>
2018-12-10 16:06:27 +0800

    。。

--- jpsite-client/src/main/java/com/mty/jpsite/configuration/ClientAuthorizeConfigProvider.java
+++ jpsite-client/src/main/java/com/mty/jpsite/configuration/ClientAuthorizeConfigProvider.java
@@ -1,21 +1,23 @@
 package com.mty.jpsite.configuration;
 
-import com.mty.jpsite.security.core.authorize.AuthorizeConfigManager;
+import com.mty.jpsite.security.core.authorize.AuthorizeConfigProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
+import org.springframework.stereotype.Component;
 
 /**
  * 自定义权限拦截配置
  */
-public class ClientAuthorizeConfigProvider implements AuthorizeConfigManager {
+@Component
+public class ClientAuthorizeConfigProvider implements AuthorizeConfigProvider {
     /**
      * @param config
      */
     @Override
-    public void config(ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry config) {
+    public boolean config(ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry config) {
         config.antMatchers("/user/regist", "/actuator/*")
-                .hasRole("ADMIN")
+                .hasRole("VIP")
                 .anyRequest().permitAll();
-
+        return false;
     }
 }
