b1bd73177a510293bdde4c9cf4bd5aaca55d9144
蒋鹏 <jiangpeng@iplas.com.cn>
2018-12-20 16:48:10 +0800

    角色权限

--- jpsite-server/src/main/java/com/mty/jpsite/server/controller/access/AccessController.java
+++ jpsite-server/src/main/java/com/mty/jpsite/server/controller/access/AccessController.java
@@ -1,9 +1,17 @@
 package com.mty.jpsite.server.controller.access;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.mty.jpsite.server.entity.access.Access;
+import com.mty.jpsite.server.entity.role.Role;
+import com.mty.jpsite.server.service.access.AccessService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -13,9 +21,48 @@ import org.springframework.web.bind.annotation.RestController;
  * @author jiangpeng
  * @since 2018-12-10
  */
+@Api(description = "权限")
 @RestController
 @RequestMapping("/access")
 public class AccessController {
+    @Autowired
+    private AccessService accessService;
 
+    @ApiOperation(value = "", notes = "保存权限")
+    @PostMapping()
+    public void save(@ApiParam(name = "access", value = "权限实体", required = true)
+                     @RequestBody Access access) {
+        accessService.save(access);
+    }
+
+    @ApiOperation(value = "/{id}", notes = "根据权限id删除")
+    @DeleteMapping("/{id}")
+    public void delete(@ApiParam(name = "id", value = "主键id", required = true)
+                       @PathVariable("id") int id) {
+        accessService.removeById(id);
+    }
+
+    @ApiOperation(value = "", notes = "编辑权限信息")
+    @PutMapping()
+    public void update(@ApiParam(name = "access", value = "权限信息", required = true)
+                       @RequestBody Access access) {
+        accessService.updateById(access);
+    }
+
+    @ApiOperation(value = "", notes = "查询所有权限")
+    @GetMapping()
+    public List<Role> findAll(@ApiParam(name = "id", value = "权限id")
+                              @RequestParam(defaultValue = "0", value = "id") Integer id,
+                              @ApiParam(name = "status", value = "该记录是否有效1：有效、0：无效")
+                              @RequestParam(defaultValue = "-1", value = "status") Integer status) {
+        QueryWrapper queryWrapper = new QueryWrapper();
+        if (id > 0) {
+            queryWrapper.eq(" ", id);
+        }
+        if (status > -1) {
+            queryWrapper.eq("status", status);
+        }
+        return accessService.list(queryWrapper);
+    }
 }
 
--- jpsite-server/src/main/java/com/mty/jpsite/server/controller/roleAccess/RoleAccessController.java
+++ jpsite-server/src/main/java/com/mty/jpsite/server/controller/roleAccess/RoleAccessController.java
@@ -1,9 +1,18 @@
 package com.mty.jpsite.server.controller.roleAccess;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.mty.jpsite.server.entity.role.Role;
+import com.mty.jpsite.server.entity.roleAccess.RoleAccess;
+import com.mty.jpsite.server.service.role.RoleService;
+import com.mty.jpsite.server.service.roleAccess.RoleAccessService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
 /**
  * <p>
@@ -14,9 +23,43 @@ import org.springframework.web.bind.annotation.RestController;
  * @author jiangpeng
  * @since 2018-12-10
  */
+@Api(description = "角色权限")
 @RestController
 @RequestMapping("/roleAccess")
 public class RoleAccessController {
+    @Autowired
+    private RoleAccessService roleAccessService;
 
+    @ApiOperation(value = "", notes = "保存角色权限")
+    @PostMapping()
+    public void save(@ApiParam(name = "roleAccess", value = "角色权限实体", required = true)
+                     @RequestBody RoleAccess roleAccess) {
+        roleAccessService.save(roleAccess);
+    }
+
+    @ApiOperation(value = "/{id}", notes = "根据角色权限id删除")
+    @DeleteMapping("/{id}")
+    public void delete(@ApiParam(name = "id", value = "主键id", required = true)
+                       @PathVariable("id") int id) {
+        roleAccessService.removeById(id);
+    }
+
+    @ApiOperation(value = "", notes = "编辑角色权限")
+    @PutMapping()
+    public void update(@ApiParam(name = "roleAccess", value = "角色信息", required = true)
+                       @RequestBody RoleAccess roleAccess) {
+        roleAccessService.updateById(roleAccess);
+    }
+
+    @ApiOperation(value = "", notes = "查询所有角色权限")
+    @GetMapping()
+    public List<Role> findAll(@ApiParam(name = "roleId", value = "角色id")
+                              @RequestParam(defaultValue = "0", value = "roleId") Integer roleId) {
+        QueryWrapper queryWrapper = new QueryWrapper();
+        if (roleId > 0) {
+            queryWrapper.eq("role_id", roleId);
+        }
+        return roleAccessService.list(queryWrapper);
+    }
 }
 
